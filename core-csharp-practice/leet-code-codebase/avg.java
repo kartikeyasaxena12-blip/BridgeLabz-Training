adead98d650c47d2fb176753e9bf38d0e87de439
kartikeya saxena <122550029+kartikeyasaxen@users.noreply.github.com>
2025-12-18 16:37:37 +0530

    [Kartikeya Saxena] add: 5 questions added in leetcode codebase

---
+++ core-csharp-practice/leet-code-codebase/avg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class avg
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double n1=sc.nextInt();
+        double n2=sc.nextInt();
+        double n3=sc.nextInt();
+        double avg= (n1+n2+n3)/3;
+        System.out.println(avg);
+        }
+}
---
+++ core-csharp-practice/leet-code-codebase/greatoftwo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class greatoftwo
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double n1=sc.nextInt();
+        double n2=sc.nextInt();
+        if(n1>n2)
+        {
+            System.out.println(n1);
+        }
+        else
+        {
+            System.out.println(n2);
+        }
+        
+        }
+}
