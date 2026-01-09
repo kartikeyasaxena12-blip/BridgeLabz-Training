6ffa6eb44c6129819abefb9ed4879eab2f96f3e6
kartikeya saxena <122550029+kartikeyasaxen@users.noreply.github.com>
2026-01-09 15:26:24 +0530

    [Kartikeya Saxena] add: added scenario based problem

---
+++ datastructure-csharp-practice/scenario-based/ATM Dispenser/ATMDispense.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabTraining.csharp_codes.ATM_Dispenser
+{
+    public class ATMDispense
+    {
+        public void Dispense(int amount, int[] notes)
+        {
+            int originalAmount = amount;
+
+            Console.WriteLine("\nDispensing Amount: Rs" + amount);
+
+            foreach (int note in notes)
+            {
+                if (amount >= note)
+                {
+                    int count = amount / note;
+                    amount = amount % note;
+
+                    Console.WriteLine("Rs" + note + " x " + count);
+                }
+            }
+
+            if (amount != 0)
+            {
+                Console.WriteLine("\nExact amount cannot be dispensed.");
+                Console.WriteLine("Fallback Amount: Rs" + (originalAmount - amount));
+            }
+            else
+            {
+                Console.WriteLine("\nCash Dispensed Successfully");
+            }
+        }
+    }
+}
---
+++ datastructure-csharp-practice/scenario-based/ATM Dispenser/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabTraining.csharp_codes.ATM_Dispenser
+{
+    public class Menu
+    {
+        static void Main()
+        {
+            ATMDispense atm = new ATMDispense();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\n====== ATM MENU ======");
+                Console.WriteLine("1. Scenario A (All Notes Available)");
+                Console.WriteLine("2. Scenario B (Rs500 Note Removed)");
+                Console.WriteLine("3. Scenario C (Fallback Case)");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("Enter choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nEnter amount to withdraw");
+                int money;
+
+                switch (choice)
+                {
+                    case 1:
+                        money = Convert.ToInt32(Console.ReadLine());
+                        atm.Dispense(money, new int[] { 500, 200, 100, 50, 20, 10, 5, 2, 1 });
+                        break;
+
+                    case 2:
+                        money = Convert.ToInt32(Console.ReadLine());
+                        atm.Dispense(money, new int[] { 200, 100, 50, 20, 10, 5, 2, 1 });
+                        break;
+
+                    case 3:
+                        money = Convert.ToInt32(Console.ReadLine());
+                        atm.Dispense(money, new int[] { 500,200,100,50, 20, 10 });
+                        break;
+
+                    case 4:
+                        exit = true;
+                        Console.WriteLine("Thank you for using ATM.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            } 
+        }
+    }
+}
