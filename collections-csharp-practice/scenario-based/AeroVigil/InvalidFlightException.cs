d79c9d14a95db0671701f5e964fe22cfe9fc05f9
kartikeya saxena <122550029+kartikeyasaxen@users.noreply.github.com>
2026-02-02 14:49:26 +0530

    [Kartikeya Saxena] add: added scenario-based problem

---
+++ collections-csharp-practice/scenario-based/AeroVigil/InvalidFlightException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabTraining.csharp_codes.AeroVigil
+{
+    public class InvalidFlightException : Exception
+    {
+        public InvalidFlightException(string message) : base(message)
+        {
+        }
+    }
+}
